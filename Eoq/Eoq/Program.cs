490691a1dad78014dd0014ec0fac215023dd67c9
Kauã Amorim <kauaqw321@gmail.com>
2019-07-13 19:30:46 -0300

    Trabalhando com valores async com Task

--- Eoq/Eoq/Program.cs
+++ Eoq/Eoq/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Eoq
 {
+    class MyClass
+    {
+        
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var open = new ProcessStartInfo("paint.exe");
+            
+            Process.Start(open);
         }
     }
 }
---
+++ ThreadsComEscolhadeQuantidade/ThreadsComEscolhadeQuantidade/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadsComEscolhadeQuantidade
+{
+    class Program
+    {
+        static List<ListaThreads> listaThreads = new List<ListaThreads>();
+        static long indice = 0;
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(IncrementIdex);
+            t1.Start();
+
+            Thread t2 = new Thread(IncrementIdex);
+            t2.Start();
+
+            Thread t3 = new Thread(IncrementIdex);
+            t3.Start();
+
+            var inicio = DateTime.Now;
+
+            while(indice < 1000) { }
+
+            var tempoTotal = DateTime.Now - inicio;
+
+            Console.WriteLine($"Tempo para execução: {tempoTotal}");
+            Console.ReadKey();
+        }
+
+        public static void IncrementIdex()
+        {
+            while (indice < 1000)
+                indice++;
+        }
+
+        public static void CarregaLista()
+        {
+            for (long i = 0; i < 1000; i++)
+            {
+                try
+                {
+                    listaThreads.Add(new ListaThreads()
+                    {
+                        Numero = indice++
+                    });
+                }
+                catch
+                {
+                    ///EstourouIndice
+                }
+            }
+        }
+    }
+
+    public class ListaThreads
+    {
+        /// <summary>
+        /// Numero que define a ordem de criacao
+        /// </summary>
+        public long Numero { get; set; } = 0;
+        /// <summary>
+        /// Identificador booleano que mostra se foi atualizado
+        /// </summary>
+        public bool Atualizado { get; set; } = false;
+    }
+}
