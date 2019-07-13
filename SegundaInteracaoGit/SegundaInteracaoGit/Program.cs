bb782e6c4567e116d0619a1600b80c00ee083bbd
Kauã Amorim <kauaqw321@gmail.com>
2019-07-13 16:06:40 -0300

    Segunda Interação com o GitHub

---
+++ SegundaInteracaoGit/SegundaInteracaoGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundaInteracaoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Segundo Projeto de Interacao com GitHub
+            Console.WriteLine("Basic GitHub");
+        }
+    }
+}
